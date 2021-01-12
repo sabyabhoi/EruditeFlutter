469d9fcc7ba85a8bc705d85089eaaf6ff368f1f4
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2021-01-12 17:45:46 +0530

    Integrated dictionary api

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,17 @@ void main() {
   ));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/word.dart
+++ lib/word.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class WordData {
   String word, partOfSpeech, definition, usage;
 
   WordData({this.word, this.partOfSpeech, this.definition, this.usage});
+
+  static Future<WordData> getWordData() async {
+    final String word = "hello";
+    Response response =
+        await get('https://api.dictionaryapi.dev/api/v2/entries/en/$word');
+    Map data = jsonDecode(response.body)[0];
+
+    final String partOfSpeech = data['meanings'][0]['partOfSpeech'];
+    final String definition =
+        data['meanings'][0]['definitions'][0]['definition'];
+    final String usage = data['meanings'][0]['definitions'][0]['example'];
+
+    final wordData = WordData(
+        word: word,
+        partOfSpeech: partOfSpeech,
+        definition: definition,
+        usage: usage);
+    return wordData;
+  }
 }
 
 class Word extends StatefulWidget {
@@ -12,6 +33,18 @@ class Word extends StatefulWidget {
 }
 
 class _WordState extends State<Word> {
+  WordData word;
+
+  @override
+  void initState() {
+    WordData.getWordData().then((value) {
+      setState(() {
+        word = value;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,24 +54,24 @@ class _WordState extends State<Word> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Text(
-            "Elucidate",
+            this.word.word,
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
           ),
           Padding(
             padding: const EdgeInsets.only(bottom: 8.0),
             child: Text(
-              "transitive verb",
+              this.word.partOfSpeech,
               style: TextStyle(fontSize: 18),
             ),
           ),
           Text(
-            "Make (something) clear; explain.",
+            this.word.definition,
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
           Text(
-            "\"work such as theirs will help to elucidate this matter\"",
+            this.word.usage,
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
           ),
